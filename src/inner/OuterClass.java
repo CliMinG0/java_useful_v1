d4dfd3e62226e0a0309e58220c843b968cc7ddce
조현진 <jhyeonjin365@gmail.com>
2025-04-25 13:13:37 +0900

    commit1

---
+++ src/inner/OuterClass.java
@@ -0,0 +1,25 @@
+package inner;
+
+public class OuterClass {
+
+    private int num = 10;
+    //inner 클래스란 클래스 안 클래스를 말한다
+    //멤버 내부 클래스
+    class InnerClass{
+        public  void display(){
+            System.out.println("inner class display() 호출" + num);
+        }
+
+    }//inned class end
+
+    public static void main(String[] args) {
+
+        //InnerClass innerClass = new InnerClass();
+        OuterClass outerClass = new OuterClass();
+        OuterClass.InnerClass innerClass = outerClass.new InnerClass();
+        innerClass.display();
+
+
+    }
+
+}//outer class end
